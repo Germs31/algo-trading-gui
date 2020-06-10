62cf0138ac6f1ffeba983f921bdfea3c4a008e15
German Nunez <german90nunez@gmail.com>
2020-06-10 12:02:10 -0700

    cleaned house, created header and sidemenu comp, node-ass and react-icons added

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1>Algo Trading</h1>
+                <nav>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
