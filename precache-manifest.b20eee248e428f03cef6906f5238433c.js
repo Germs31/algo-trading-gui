734ad1cfefc4afdad0b534cbdc3ec6eb7d28d790
German Nunez <german90nunez@gmail.com>
2020-06-11 00:24:13 -0700

    Updates

---
+++ precache-manifest.b20eee248e428f03cef6906f5238433c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b781f6b88d3f2fb988e04e44573e1c57",
+    "url": "/algo-trading-gui/index.html"
+  },
+  {
+    "revision": "b19cc2dbe730ba13813a",
+    "url": "/algo-trading-gui/static/css/main.acfc774c.chunk.css"
+  },
+  {
+    "revision": "f18a8abca0890af6c0d5",
+    "url": "/algo-trading-gui/static/js/2.894463ac.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/algo-trading-gui/static/js/2.894463ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19cc2dbe730ba13813a",
+    "url": "/algo-trading-gui/static/js/main.d7df9b46.chunk.js"
+  },
+  {
+    "revision": "0e7cbeb821eecb54acb3",
+    "url": "/algo-trading-gui/static/js/runtime-main.69db31b4.js"
+  }
+]);
